8d54ccd602fb7e64ed361518dafa836aa92fe752
Robert <robgladue@hotmail.com>
2022-03-13 14:02:35 -0600

    add DB table changes/city Information

--- server/db/schema/01_create_users.sql
+++ server/db/schema/01_create_users.sql
@@ -1,22 +1,31 @@
 -- schema/01_create_users.sql
-DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS cities CASCADE;
+DROP TABLE IF EXISTS user CASCADE;
+DROP TABLE IF EXISTS city CASCADE;
 DROP TABLE IF EXISTS comments CASCADE;
 -- CREATE USERS
-CREATE TABLE users (
+CREATE TABLE user (
   id SERIAL PRIMARY KEY,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255)
 );
  
-CREATE TABLE cities (
+CREATE TABLE city (
 id SERIAL PRIMARY KEY,
 name VARCHAR(255) NOT NULL,
-rating integer NOT NULL
+img_link VARCHAR(255),
+country VARCHAR(255),
+language VARCHAR(255),
+population INT
+avg_temp INT,
+cost_of_living VARCHAR(255),
+internet_speed VARCHAR(255),
+safety_rating VARCHAR(255)
 );
 
-CREATE TABLE comments (
+CREATE TABLE user_inputs (
   id SERIAL PRIMARY KEY,
   user_id VARCHAR(255) NOT NULL,
-  description VARCHAR(255) NOT NULL
+  city_id INT,
+  comment VARCHAR(255) NOT NULL,
+  rating INT
 );
